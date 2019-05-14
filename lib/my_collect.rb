ab376a29819ab6b5e848599ef3e32654f43ece2f
Tam Vu <vutam_a@hotmail.com>
2019-05-14 00:30:20 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def my_collect(array)
   i = 0
   collection = []
@@ -7,5 +8,12 @@ def my_collect(array)
   end
   collection
 
+=======
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(collection)
+  my_collect(collection) do |lang|
+    lang.upcase
+  end 
+>>>>>>> cec8ea126261de00702c620c53cf30116e260f79
 end 
 
